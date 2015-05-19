75bfe8a679c7c42b62055bae5305cdb76e170b55
Martin Bontrager <mbontrager@gmail.com>
2015-05-19 12:35:50 -0500

    first commit

---
+++ R/flavo.R
@@ -0,0 +1,57 @@
+## Martin Bontrager
+# May 19, 2015
+# Flavobacterium analysis from MOTHUR output
+
+library(phyloseq)
+library(ggplot2)
+library(RColorBrewer)
+
+setwd("D:/Users/Martin/Dropbox/Projects/flavobacterium/data/")
+
+## Read in the .csv otu table and metadata
+otuTable <- read.csv("2015-05-13_taxonomy_subtable.csv", header = TRUE, row.names = 1)
+metaData <- read.csv("16S_metadata.csv", header = TRUE, row.names = 1)
+ordered_samples <- as.vector((read.table("samples.txt"))[, 1])
+
+## Create matrices for phyloseq
+otumat <- as.matrix(subset(otuTable, select=c(8:length(colnames(otuTable)))))
+otuTable[] <- lapply(otuTable, as.character)
+taxmat <- as.matrix(subset(otuTable, select=c(1:7)))
+
+## Declare phyloseq objects
+OTU <- otu_table(otumat, taxa_are_rows = TRUE)
+TAX <- tax_table(taxmat)
+metaData <- sample_data(metaData)
+
+physeq <- phyloseq(OTU, TAX, metaData)
+
+## GGplot themeing
+theme_set(theme_bw())
+pal = "Set1"
+scale_colour_discrete <- function(palname = pal, ...) {
+        scale_colour_brewer(palette = palname, ...)
+}
+scale_fill_discrete <- function(palname = pal, ...) {
+        scale_fill_brewer(palette = palname, ...)
+}
+getPalette <- colorRampPalette(brewer.pal(9, "Set1"))
+
+## Prune taxa not present in any sample (if they exist)
+## And remove unnecessary objects from memory
+GP <- prune_taxa(taxa_sums(physeq) > 0, physeq)
+rm(otuTable, otumat, taxmat, physeq, TAX, OTU)
+
+## Flavobacterium abundance plots
+GP_sub1 <- subset_taxa(GP, Genus == "Flavobacterium")
+theme_set(theme_bw() +
+                  theme(axis.line = element_line(colour = "black"),
+                        panel.grid.major = element_blank(),
+                        panel.grid.minor = element_blank(),
+                        panel.border = element_blank(),
+                        panel.background = element_blank()))
+plot_bar(GP_sub1, fill = "Species", title = "Flavobacterium") + 
+        scale_x_discrete(limits = ordered_samples) + 
+        theme(axis.title.x = element_text(size = 12),
+              axis.title.y = element_text(size = 12),
+              plot.title = element_text(size = 24, face = "bold"))
+
